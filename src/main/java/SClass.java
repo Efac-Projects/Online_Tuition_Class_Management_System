4729fbd2bd2c73652ad2872fea7d3d315b57d64c
Kethaki Dissanayake <60033672+Kethaki-Dissanayake@users.noreply.github.com>
2021-10-14 23:55:22 +0530

    2nd Commit

--- src/main/java/SClass.java
+++ src/main/java/SClass.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class SClass {
+import java.util.Scanner;
+
+public class SClass {
+    private int Year;
+    private String Name;
+    private String TeacherName;
+
+    public SClass() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Class Year: ");
+        this.Year = Integer.valueOf(in.nextLine());
+
+        System.out.print("Enter Class: ");
+        this.Name = in.nextLine();
+
+        System.out.print("Enter Teacher Name: ");
+        this.TeacherName = in.nextLine();
+    }
+
+    public int getYear() {
+        return Year;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getTeacherName() {
+        return TeacherName;
+    }
 }
