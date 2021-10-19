11701f22570d3193a7f8ee7e19408ca31a9114f1
Affin Ajanthan <59964903+Affin-Ajanthan@users.noreply.github.com>
2021-10-19 15:45:40 +0530

    Update InMemorySessionDBTest.java

--- src/test/java/InMemorySessionDBTest.java
+++ src/test/java/InMemorySessionDBTest.java
@@ -1,10 +1,6 @@
-
-/*
-import Class.SClass;
-import Class.ClassService;
-import ClassAssignment.ClassAssignmentService;
-import Student.Student;
+import Session.InMemorySessionDB;
 import Session.Session;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -20,16 +16,39 @@ import static org.mockito.Mockito.*;
 public class InMemorySessionDBTest {
 
     @Test
-    public void want_to_store_session_when_create(){
+    public void want_to_return_empty(){
+        InMemorySessionDBTestDB inMemoryStudentDB = new InMemorySessionDBTest();
+        assertThat(inMemorySessionDB.getSession(),is(empty()));
 
-        ClassService classes = mock(ClassService.class);
-        doNothing().when(classes).CreateClass("Math","Joy",10);
-        SClass sclass = new Sclass("11","Math","John");
-        //Session session = new Session("19-10-2021", "1pm - 2pm");
-        Session session = new Session("19-10-2021","1pm-2pm");
-        InMemorySessionDBTest store = new InMemorySessionDBTest(classes);
-
-        store.storeSession(session);
-        assertThat(store.getSessions(), hasSize(1));
     }
+
+    @Test
+    public void want_to_store_session_when_session_is_given(){
+      Session session = mock(Session.class);
+      InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
+      inMemorySessionDB.storeSession(session);
+      assertThat(inMemoryStudentDB.getSession(),hasSize(1));
+
+    }
+    @Test
+    public void want_to_contain_session_when_create(){
+        Session session = new Session("19-10-2021","1pm-2pm");
+        InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
+        inMemorySessionDB.storeSession(session);
+        assertThat(inMemoryStudentDB.getSession(),contains(session));
+
+    }
+
+    @Test
+    public void want_to_store_all_session_for_multiple_creation(){
+        Session session1 = new Session("19-10-2021","1pm-2pm");
+        Session session2 = new Session("20-10-2021","3pm-4pm");
+        InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
+        inMemorySessionDB.storeSession(session1);
+        inMemorySessionDB.storeSession(session2);
+        assertThat(inMemoryStudentDB.getSession(),containsInAnyOrder(session1,session2));
+
+    }
+
+
 }
