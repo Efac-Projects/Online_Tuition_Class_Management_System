084ad5627372f0b8478cc862e56eab9b67700150
Affin Ajanthan <59964903+Affin-Ajanthan@users.noreply.github.com>
2021-10-19 17:48:06 +0530

    Update InMemorySessionDBTest.java

--- src/test/java/Session/InMemorySessionDBTest.java
+++ src/test/java/Session/InMemorySessionDBTest.java
@@ -1,8 +1,10 @@
 package Session;
 
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.Test;
 import Session.InMemorySessionDB;
 
+import java.io.Console;
 import java.util.*;
 import java.util.Arrays;
 
@@ -13,23 +15,22 @@ import static org.mockito.Mockito.*;
 
 class InMemorySessionDBTest {
 
-
     @Test
     public void want_to_return_empty(){
         InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
-        assertThat(inMemorySessionDB.getSessions(),is(empty()));
-
+        assertThat(inMemorySessionDB.getSessions().size(),is(0));
     }
 
     @Test
     public void want_to_store_session_when_session_is_given(){
-        Session session1 = mock(Session.class);
-        Session session2 = mock(Session.class);
+        InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
+        Session session1 = new Session("19-10-2021","1pm-2pm");
+        Session session2 = new Session("20-10-2021","3pm-4pm");
         List<Session> Sessions = Arrays.asList(session1,session2);
         String sessionName="sessionName";
-        InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
         inMemorySessionDB.storeSession(sessionName,Sessions);
-        assertThat(inMemorySessionDB.getSessions(),hasSize(1));
+
+        assertThat(inMemorySessionDB.getSessions().size(),is(1));
 
     }
     @Test
@@ -40,7 +41,10 @@ class InMemorySessionDBTest {
         String sessionName="sessionName";
         InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
         inMemorySessionDB.storeSession(sessionName,Sessions);
-        assertThat(inMemorySessionDB.getSessions(),contains(Sessions));
+
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasEntry(sessionName,Sessions));
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasKey(sessionName));
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasValue(Sessions));
 
     }
 
@@ -50,14 +54,25 @@ class InMemorySessionDBTest {
         Session session2 = new Session("20-10-2021","3pm-4pm");
         Session session3 = new Session("19-10-2021","1pm-2pm");
         Session session4 = new Session("20-10-2021","3pm-4pm");
+
         InMemorySessionDB inMemorySessionDB = new InMemorySessionDB();
         List<Session> Sessions1 = Arrays.asList(session1,session2);
-        String sessionName1="sessionName";
+        String sessionName1="sessionName1";
+
         List<Session> Sessions2 = Arrays.asList(session3,session4);
-        String sessionName2="sessionName";
+        String sessionName2="sessionName2";
+
         inMemorySessionDB.storeSession(sessionName1,Sessions1);
         inMemorySessionDB.storeSession(sessionName2,Sessions2);
-        assertThat(inMemorySessionDB.getSessions(),containsInAnyOrder(Sessions1,Sessions2));
+
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasEntry(sessionName1,Sessions1));
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasEntry(sessionName2,Sessions2));
+
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasKey(sessionName1));
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasValue(Sessions1));
+
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasKey(sessionName2));
+        assertThat(inMemorySessionDB.getSessions(), IsMapContaining.hasValue(Sessions2));
 
     }
 }
